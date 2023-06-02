5de1559fbae4dd1377a663e515ba5f9c003ef215
Jason Babie <jbabie@imxsoftware.com>
2023-06-02 07:32:36 +0200

    Add validation rules to validate longitude and latitude values

--- creating-small-tools/src/geo2json/geo2json-main.c
+++ creating-small-tools/src/geo2json/geo2json-main.c
@@ -21,6 +21,14 @@ int main() {
 			printf(",\n");
 		else
 			started = 1;
+		if (latitude < -90.0 || latitude > 90.0) {
+			printf("Invalid latitude: %f\n", latitude);
+			return 2;
+		}
+		else if (longitude < -180.0 || longitude > 180.0) {
+			printf("Invalid longitude: %f\n", longitude);
+			return 2;
+		}
 		printf("{latitude: %f, longitude: %f, info '%s'}", latitude, longitude, info);
 	}
 	puts("\n]");
